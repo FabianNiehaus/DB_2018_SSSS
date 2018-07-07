1b916e2642a6f6b4d2df90ad2ac6b49cf8648db4
Unknown <zeit_geist@posteo.de>
2018-07-07 18:10:25 +0200

    Auto stash before merge of "development" and "origin/development"

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,18 +1,27 @@
+// Variables
 var conn;
+var classHighlight = 'highlight';
+
+//Methods
 
 function connect() {
     conn = new WebSocket("ws://localhost:8080/BuzzwordBingo/actions");
-    conn.onmessage = function(msg) {
-        cValue = document.getElementById("out").value;
-        document.getElementById("out").value = cValue + msg.data + "\n";
+
+    conn.onmessage = function (msg) {
+
+        cValue = document.getElementById("info").value;
+
+        document.getElementById("info").value = cValue + msg.data + "\n";
+
     }
+
     document.getElementById("connect").setAttribute("disabled", "disabled");
     document.getElementById("disconnect").removeAttribute("disabled");
 }
 
 function disconnect() {
     conn.close();
-    document.getElementById("out").value = "";
+    document.getElementById("info").value = "";
     document.getElementById("disconnect").setAttribute("disabled", "disabled");
     document.getElementById("connect").removeAttribute("disabled");
 }
@@ -22,17 +31,26 @@ function sendMessage() {
 }
 
 // Sends the presed tiles as a String to the Server
-function sendAction (action) {
+function sendAction(action) {
     conn.send(action)
 }
 
-window.addEventListener("DOMContentLoaded", function() {
-    let tables = document.querySelectorAll(".table");
+window.addEventListener("DOMContentLoaded", function () {
 
-    Array.from(tables, function(table) {
-        table.addEventListener("click", function() {
-            alert(this.classList[1]);
-            sendAction(this.classList[1]);
+    var $hwords = $('.word').click(function (e) {
+        e.preventDefault();
+
+        $(this).addClass(classHighlight);
+
+    });
+
+    let words = document.querySelectorAll(".word");
+
+    Array.from(words, function (word) {
+
+        word.addEventListener("click", function () {
+            //alert(this.classList[1]);
+            //sendAction(this.classList[1]);
         });
     });
 });
