38eab27f19d6bc13660fa9be73fca9555c2ddc90
Tuyet <anhtuyetnguyen94@gmail.com>
2018-07-07 19:41:50 +0200

    deleteRequest

--- Buzzword Bingo/js/scripts.js
+++ Buzzword Bingo/js/scripts.js
@@ -1,34 +1,17 @@
  <!-- https://www.youtube.com/watch?v=wSnELgwFFiQ-->
  
       
-   $(document).ready (function addWord(){
-        var text = $("#new-text").val();
-        $("#buzzwordbingo").append('<td><input type = "checkbox" class = "done" /> '+text+' <button class = "delete">Delete</button></td>');
-        $("new-text").val('');
-    });
-   $(document).ready (function deleteWord(){
-        $(this).parent().remove();
-    });
-   $(document). function finishWord(){
-        if ($(this).parent().css('textDecoration')== 'line-through'){
-            $(this).parent().css('textDecoration', 'none');
-        }else{
-            $(this).parent().css('textDecoration', 'line-through');
-        }
+  $(function(){
+  var $curParent, Content;
+  $(document).delegate("span","click", function(){
+    if($(this).closest("s").length) {
+      Content = $(this).parent("s").html();
+      $curParent = $(this).closest("s");
+      $(Content).insertAfter($curParent);
+      $(this).closest("s").remove();
     }
-   $(document). $(function(){
-        $("#add").on('click', addWord);
-        $(document).on('click', '.delete', deleteWord);
-        $(document).on('click', '.done', finishWord);
-    });
-    $('#myTable tr').click(function(){
-    $(this).remove();
-    return false;
-    });
-
-    $(".delete").live('click', function(event) {
-    $(this).parent().parent().remove();
-    });
-
-
-    $($(this).closest("tr")).remove()
+    else {
+      $(this).wrapAll("<s />");
+    }
+  });
+});
