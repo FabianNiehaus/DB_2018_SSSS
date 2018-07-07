34c5cbddc21bacc5a4d2658401afc4967c93c52b
fabian.niehaus.94@gmail.com <starwars7>
2018-07-07 12:49:29 +0200

    Added: Tuyets Website-Entwurf

---
+++ html-draft/js/scripts1.js
@@ -0,0 +1,28 @@
+$(document).function addListItem() {
+    'use strict';
+    var text = $("#new-text").val();
+    $("#kategori").append('<p><input type="checkbox" class="done" /> ' + text + '  <button class="delete">Delete</button></p>');
+    $("#new-text").val('');
+}
+
+$(document).function deleteItem() {
+    'use strict';
+    $(this).parent().remove();
+}
+
+$(document).function finishItem() {
+    'use strict';
+    if ($(this).parent.css('textDecoration') === 'line-through') {
+        $(this).parent().css('textDecoration', 'none');
+        } else {
+            $(this).parent().css('textDecoration', 'line-through');
+        }
+}
+
+$(document).$(function () {
+    'use strict';
+    $("#add").on('click', addListItem);
+    $(document).on('click', '.delete', deleteItem);
+    $(document).on('click', '.done', finishItem);
+});
+
