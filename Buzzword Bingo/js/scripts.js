df4aa9fd2123eb18613be7d208d7bb614556b51b
Tuyet <anhtuyetnguyen94@gmail.com>
2018-07-07 18:26:10 +0200

    mit JavaScript

--- Buzzword Bingo/js/scripts.js
+++ Buzzword Bingo/js/scripts.js
@@ -1,23 +1,34 @@
  <!-- https://www.youtube.com/watch?v=wSnELgwFFiQ-->
  
       
-    function addWord(){
+   $(document).ready (function addWord(){
         var text = $("#new-text").val();
         $("#buzzwordbingo").append('<td><input type = "checkbox" class = "done" /> '+text+' <button class = "delete">Delete</button></td>');
         $("new-text").val('');
-    }
-    function deleteWord(){
+    });
+   $(document).ready (function deleteWord(){
         $(this).parent().remove();
-    }
-    function finishWord(){
+    });
+   $(document). function finishWord(){
         if ($(this).parent().css('textDecoration')== 'line-through'){
             $(this).parent().css('textDecoration', 'none');
         }else{
             $(this).parent().css('textDecoration', 'line-through');
         }
     }
-    $(function(){
+   $(document). $(function(){
         $("#add").on('click', addWord);
         $(document).on('click', '.delete', deleteWord);
         $(document).on('click', '.done', finishWord);
     });
+    $('#myTable tr').click(function(){
+    $(this).remove();
+    return false;
+    });
+
+    $(".delete").live('click', function(event) {
+    $(this).parent().parent().remove();
+    });
+
+
+    $($(this).closest("tr")).remove()
--- Buzzword Bingo/js/scripts1.js
+++ Buzzword Bingo/js/scripts1.js
@@ -1,16 +1,17 @@
-$(document).function addListItem() {
+jQuery.noConflict();
+function addListItem() {
     'use strict';
     var text = $("#new-text").val();
     $("#kategori").append('<p><input type="checkbox" class="done" /> ' + text + '  <button class="delete">Delete</button></p>');
     $("#new-text").val('');
 }
 
-$(document).function deleteItem() {
+function deleteItem() {
     'use strict';
     $(this).parent().remove();
 }
 
-$(document).function finishItem() {
+function finishItem() {
     'use strict';
     if ($(this).parent.css('textDecoration') === 'line-through') {
         $(this).parent().css('textDecoration', 'none');
@@ -19,10 +20,15 @@ $(document).function finishItem() {
         }
 }
 
-$(document).$(function () {
+$ function () {
     'use strict';
     $("#add").on('click', addListItem);
     $(document).on('click', '.delete', deleteItem);
     $(document).on('click', '.done', finishItem);
+});
+
+$('#myTable tr').click(function(){
+    $(this).remove();
+    return false;
 });
 
