1bee4e0b7a0e075da1db0a8d9b9daef0e4772dab
Unknown <jasser.saad@hotmail.com>
2018-07-08 17:57:00 +0200

    reformat data code

--- src/main/java/data/Buzzword.java
+++ src/main/java/data/Buzzword.java
@@ -2,15 +2,15 @@ package data;
 
 public class Buzzword {
 
-    public String get() {
-        return buzzword;
-    }
-
-
     private String buzzword;
+
 
     public Buzzword(String buzzword) {
         this.buzzword = buzzword;
+    }
+
+    public String get() {
+        return buzzword;
     }
 
     public String getBuzzword() {
--- src/main/java/data/Player.java
+++ src/main/java/data/Player.java
@@ -4,6 +4,17 @@ public class Player {
 
     private int id;
     private String username;
+    private String loginname;
+    private String password;
+    private int score;
+    private boolean isAdmin;
+
+    public Player(int id, String username, String loginname, String password) {
+        this.id = id;
+        this.username = username;
+        this.loginname = loginname;
+        this.password = password;
+    }
 
     public int getId() {
         return id;
@@ -29,25 +40,11 @@ public class Player {
         return isAdmin;
     }
 
-    private String loginname;
-    private String password;
-
-    private int score;
-
-    private boolean isAdmin;
-
     public void setAdmin(boolean admin, String pasword) {
         String functionPassword = "i3ßfnzr984jf02!";
-        if(password.equals(functionPassword)) {
+        if (password.equals(functionPassword)) {
             isAdmin = admin;
         }
-    }
-
-    public Player(int id, String username, String loginname, String password) {
-        this.id = id;
-        this.username = username;
-        this.loginname = loginname;
-        this.password = password;
     }
 
 }
